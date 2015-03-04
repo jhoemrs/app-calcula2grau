d75d6fd8df4d3614ca710b14c4b4f10fef131637
Jhonatan Mark <jhoemrs@gmail.com>
2015-03-04 15:23:08 -0300

    Primeira Versao Funcional

--- www/js/app.js
+++ www/js/app.js
@@ -17,3 +17,22 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('MyCtrl',function($scope){
+    	$scope._delta = function(){
+        return $scope.b * $scope.b - 4 * $scope.a * $scope.c;
+        }
+        $scope.delta = function(a, b, c){
+        return b*b - 4*a*c;
+        }
+        $scope.r1 = function(){
+        return (-$scope.b + Math.sqrt($scope._delta())) / (2 * $scope.a)
+        }
+        $scope.r2 = function(){
+        return (-$scope.b - Math.sqrt($scope._delta())) / (2 * $scope.a)
+        }
+    
+    $scope.meutexto_reversed = function(){
+        return $scope.meutexto.split("").reverse().join("")
+    }
+})
